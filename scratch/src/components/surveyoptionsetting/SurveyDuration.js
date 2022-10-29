665e3f57d03f49a93676db0b57a38b0aa2a3ee4e
JMINJong <wjsalswhd55@gachon.ac.kr>
2022-10-29 23:32:27 +0900

    [sc-35]add_설문 기간 로직 구현

--- scratch/src/components/surveyoptionsetting/SurveyDuration.js
+++ scratch/src/components/surveyoptionsetting/SurveyDuration.js
@@ -3,39 +3,45 @@ import React, { useState } from "react";
 
 function SurveyDuration() {
   const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(null);
+  const [endDate, setEndDate] = useState(new Date());
 
   const QuickButtonWeek=()=>{
     const beforeDate=new Date();
     const afterDate=new Date();
     setStartDate(beforeDate);
-    setEndDate(afterDate.setDate(afterDate.getDate()+7));
+    afterDate.setDate(afterDate.getDate()+7)
+    setEndDate(afterDate);    
   };
 
   const QuickButtonDoubleWeek=()=>{
     const beforeDate=new Date();
     const afterDate=new Date();
     setStartDate(beforeDate);
-    setEndDate(afterDate.setDate(afterDate.getDate()+14));
+    afterDate.setDate(afterDate.getDate()+14)
+    setEndDate(afterDate);
   };
 
   const QuickButtonMonth=()=>{
     const beforeDate=new Date();
     const afterDate=new Date();
     setStartDate(beforeDate);
-    setEndDate(afterDate.setDate(afterDate.getDate()+30));
+    afterDate.setDate(afterDate.getDate()+30)
+    setEndDate(afterDate);
   };
   return (
     <div align="center">
       <h2>2. 설문 기간 설정</h2>
+      <h3>기간을 정해주세요</h3>
       <div>
         <button onClick={QuickButtonWeek}>7일</button>
         <button onClick={QuickButtonDoubleWeek}>14일</button>
         <button onClick={QuickButtonMonth}>30일</button>
       </div>
       <div align="center">
+        <h4>시작 기간 ~ 종료 기간</h4>
         <SurveyDatePicker startDate={startDate} endDate={endDate} setStartDate={setStartDate} setEndDate={setEndDate}></SurveyDatePicker>
       </div>
+      {/* <button onClick={()=>console.log(startDate,endDate)}>테스트</button> */}
     </div>
   );
 }
--- scratch/src/components/surveyoptionsetting/surveyduration/SurveyDatePicker.js
+++ scratch/src/components/surveyoptionsetting/surveyduration/SurveyDatePicker.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, {useRef} from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 function SurveyDatePicker(props) {
   
+  let datepicker=useRef(null); 
+
   const onChange = (dates) => {
     const [start, end] = dates;
-    props.setStartDate(start);
-    props.setEndDate(end);
+    const date=new Date();
+    if(start.getDate()< date.getDate()&& start.getMonth()<=date.getMonth() ){
+      // console.log(start.getDate(),date.getDate());
+      alert("기간은 현재 날짜보다 빠를 수 없습니다!");
+      datepicker.setOpen(false);
+    }else{
+      props.setStartDate(start);
+      props.setEndDate(end);
+    }    
   };
   const ExampleCustomInput = React.forwardRef(({ value, onClick }, ref) => (
     <button className="example-custom-input" onClick={onClick} ref={ref}>
@@ -15,7 +24,8 @@ function SurveyDatePicker(props) {
     </button>
   ));
   return (
-    <DatePicker      
+    <DatePicker
+      ref={(r)=>datepicker=r}
       customInput={<ExampleCustomInput />}
       placeholderText="기간을 정해주세요"
       onChange={onChange}
