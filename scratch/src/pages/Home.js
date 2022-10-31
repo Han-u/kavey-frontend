c015fca4587613f2ce0387b8c42638673bfa1b7f
JMINJong <wjsalswhd55@gachon.ac.kr>
2022-10-31 10:17:38 +0900

    [sc-38]fix_평점 결과

--- scratch/src/pages/Home.js
+++ scratch/src/pages/Home.js
@@ -1,11 +1,12 @@
 import SurveyNarrative from "../components/Survey/SurveyNarrative";
-
+import SurveyRating from "../components/Survey/SurveyRating";
 
 function Home(){
     return (
         <div>
             대충 홈화면이라는뜻
             <SurveyNarrative/>
+            <SurveyRating/>
 
 
 
