b1722898cf567d509d8d6834469cc97440bd359b
yowon25 <yowon25@naver.com>
2022-11-06 18:14:39 +0900

    [sc-25] fix_설문지 페이지 제작

--- scratch/src/pages/Management.js
+++ scratch/src/pages/Management.js
@@ -81,10 +81,10 @@ function Management(){
             </div>
             <div style={style.body}>
                 <div style={style.btn}>
-                    <Button variant={status == 'ALL'? "contained": "outlined"} onClick={onClick} sx={{marginRight: 1}} value="ALL">전체</Button>
-                    <Button variant={status == 'MAKING'? "contained": "outlined"} onClick={onClick} sx={{marginRight: 1}} value="MAKING">제작중</Button>
-                    <Button variant={status == 'PROGRESS'? "contained": "outlined"} onClick={onClick} sx={{marginRight: 1}} value="PROGRESS">진행중</Button>
-                    <Button variant={status == 'DONE'? "contained": "outlined"} onClick={onClick} value="DONE">설문완료</Button>
+                    <Button variant={status === 'ALL'? "contained": "outlined"} onClick={onClick} sx={{marginRight: 1}} value="ALL">전체</Button>
+                    <Button variant={status === 'MAKING'? "contained": "outlined"} onClick={onClick} sx={{marginRight: 1}} value="MAKING">제작중</Button>
+                    <Button variant={status === 'PROGRESS'? "contained": "outlined"} onClick={onClick} sx={{marginRight: 1}} value="PROGRESS">진행중</Button>
+                    <Button variant={status === 'DONE'? "contained": "outlined"} onClick={onClick} value="DONE">설문완료</Button>
                 </div>
                 <div style={style.surveyContainer}>
                     <Grid container>
