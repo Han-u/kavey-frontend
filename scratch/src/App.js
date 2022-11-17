f9203e58f257b031f182d608d8ddd7f6e4c7c05e
kahyeokjin <hyeokjin977@gmail.com>
2022-11-17 15:53:26 +0900

    [SC-53]fix_라우터수정

--- scratch/src/App.js
+++ scratch/src/App.js
@@ -4,20 +4,14 @@ import {
   Routes
 } from "react-router-dom";
 import Home from "./pages/Home";
-import Survey from "./pages/Survey";
-import SurveyOptionSetting from "./pages/SurveyOptionSetting";
-import Management from "./pages/Management";
-import Participant from "./pages/Participant";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/survey" element={<Survey/>}/>
-        <Route path="/surveyoptionsetting" element={<SurveyOptionSetting/>}/>
-        <Route path="management" element={<Management/>}/>
-        <Route path="/participant" element={<Participant/>}/>
+
       </Routes>
     </Router>
   );
