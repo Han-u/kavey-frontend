6203626fdb985720cd8adf4726390915a4503cf7
han <chos9715@naver.com>
2022-12-12 03:42:39 +0900

    fix : stepper 디자인 수정

--- scratch/src/components/surveyoptionsetting/public/Stepper/StepperChild3.js
+++ scratch/src/components/surveyoptionsetting/public/Stepper/StepperChild3.js
@@ -25,21 +25,16 @@ function StepperChild3(){
       flexDirection: 'row',
       justifyContent:'space-between',
       alignItems: 'center'}}>
-          <Box sx={{width:"55px",height:"55px",backgroundColor:"rgba(255, 255, 255, 0.5)",borderRadius:"44px",alignItems: 'center',display:"flex",
+          <Box sx={{width:"50px",height:"50px",backgroundColor:"rgba(255, 255, 255, 0.5)",borderRadius:"44px",alignItems: 'center',display:"flex",
           justifyContent:'center'}} onClick={MoveFirst}>
-            <Box sx={{width:"30px",height:"30px",borderRadius:"44px",alignItems: 'center',display:"flex",
-          justifyContent:'center',border:"2px solid"}}><Typography style={{fontFamily:"NanumSquare",fontWeight:"700"}}>1</Typography>
-          </Box>
+            <Typography style={{fontFamily:"NanumSquareB",fontWeight:"700"}}>1</Typography>
         </Box>
-        <Box sx={{width:"55px",height:"55px",backgroundColor:"rgba(255, 255, 255, 0.5)",borderRadius:"44px",alignItems: 'center',display:"flex",
+        <Box sx={{width:"50px",height:"50px",backgroundColor:"rgba(255, 255, 255, 0.5)",borderRadius:"44px",alignItems: 'center',display:"flex",
           justifyContent:'center'}} onClick={MoveSecond}>
-          <Box sx={{width:"30px",height:"30px",borderRadius:"44px",alignItems: 'center',display:"flex",
-          justifyContent:'center',border:"2px solid"}}>
-            <Typography style={{fontFamily:"NanumSquare",fontWeight:"700"}}>2</Typography>
-          </Box>
+            <Typography style={{fontFamily:"NanumSquareB",fontWeight:"700"}}>2</Typography>
         </Box>
         <Box sx={{width:"210px",
-        height:"60px",
+        height:"50px",
         backgroundColor:"white",
         borderRadius:"44px",
         display: 'flex',
@@ -53,7 +48,7 @@ function StepperChild3(){
         }}>
             <Typography style={{color:"white",fontSize:"24px"}}>3</Typography>
           </Box>
-            <Typography style={{fontSize:"26px",fontFamily:"NanumSquare",fontWeight:"700"}}>설문 배포</Typography>
+            <Typography style={{fontSize:"23px",fontFamily:"NanumSquareB"}}>설문 배포</Typography>
         </Box>
       </Box>
     )
